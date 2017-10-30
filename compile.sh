82b7bd7acc911a56b3c6675eea0bd097f37db6ed
Joe Fox <joefoxdevelopment@gmail.com>
2017-10-30 14:23:16 +0000

    Add minification to compile.sh
    This removes checks for lessc existing. It will be added to the
    readme.md when I get around to writing it.

--- compile.sh
+++ compile.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v lessc)" ]; then
-	echo "The less compiler is not installed.";
-	echo "Run `npm install -g less` to install the less compiler globally.";
-	exit 1;
-fi
-
 lessc less/stickynote.less compiled-css/stickynote.css;
-exit 0;
+lessc --clean-css less/stickynote.less minified-css/stickynote.min.css;
